e87c76279a0fe13d9e600fea433d890c1d1ff2de
bro.lee <bro.lee@sparkplus.co>
2023-12-18 13:50:18 +0900

    readme 추가

---
+++ Readme.md
@@ -0,0 +1,48 @@
+# 음성 종류
+- [aws polly 목소리](https://docs.aws.amazon.com/ko_kr/polly/latest/dg/voicelist.html)
+- ko-KR
+  - Seoyeon
+- en-US
+  - FEMAIL
+    - Danielle
+    - Ivy(child)
+    - Joanna
+    - Kendra
+    - Kimberly
+    - Salli
+    - Ruth
+  - MALE
+    - Gregory
+    - Joey
+    - Justin(child)
+    - Kevin(child)
+    - Matthew
+    - Stephen
+
+# 속도 조절
+- rate 에 속도 명시
+
+```python
+
+ssml_text = """
+<speak>
+  <prosody rate="105%">
+        창세기 5장은 아담에서 노아까지 선조들을 기록하며, 그들의 족보를 자세히 나열합니다. 이러한 기록은 인간의 초기 단계와 신성한 의도에 대한 이해를 제공합니다.
+  </prosody>
+
+</speak>
+"""
+
+```
+
+- 속도명시를 하지 않는다면 일반 텍스트로 전달해도 됨
+```python
+# Request speech synthesis
+response = client_polly.synthesize_speech(
+  Text=text,
+  OutputFormat='mp3',
+  VoiceId='Matthew'  # You can change the voice here
+)
+
+
+```
--- main.py
+++ main.py
@@ -16,16 +16,35 @@ client_polly = boto3.client(
 
 # Text to synthesize
 text = """
-Hello, this is a test message for AWS Polly.
+Genesis chapter 5 records the genealogy of the ancestors from Adam to Noah, providing a detailed listing of their lineage. These records offer an understanding of the early stages of humanity and God's divine intentions.
 
+Adam, the first ancestor of humanity, was created by God. He lived with Eve in the Garden of Eden but was expelled due to disobedience. However, Adam's life didn't end there. He lived for 930 years, and his descendants played a significant role in the early history of humanity. Their long lifespans signify God's special grace and His plan for humanity.
+
+Another notable figure is Enoch. Enoch is remarkable for his unique experience. He didn't experience death but was taken up to heaven. This symbolizes the mystery of God's power and His plan.
+
+The last part of this chapter relates to Noah. His story continues in Genesis chapter 6 and covers the significant turning point in human history, the Great Flood.
+
+This chapter holds a significant place in the Bible as it deals with crucial aspects of the origin and early history of humanity, providing spiritual teachings and inspiration.
+
+"""
+
+ssml_text = """
+<speak>
+<prosody rate="105%">
+ <lang xml:lang="en-US">anyway,</lang> 창세기 5장은 아담에서 노아까지 선조들을 기록하며, 그들의 족보를 자세히 나열합니다. 이러한 기록은 인간의 초기 단계와 신성한 의도에 대한 이해를 제공합니다.
+</prosody>
+
+</speak>
 """
 
 # Request speech synthesis
 response = client_polly.synthesize_speech(
-    Text=text,
+    Text=ssml_text,
     OutputFormat='mp3',
-    VoiceId='Matthew'  # You can change the voice here
+    TextType='ssml',
+    VoiceId='Seoyeon'  # You can change the voice here
 )
+
 
 # Saving the audio
 if "AudioStream" in response:
