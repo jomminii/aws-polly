da6e0955ef8c0054f90f0c19829821e73c299386
bro.lee <bro.lee@sparkplus.co>
2024-01-23 23:26:03 +0900

    home markdown 수정

--- Home.py
+++ Home.py
@@ -1,4 +1,12 @@
 import streamlit as st
 st.title("make voice with aws polly")
-a = st.select_slider("Pick a size", ["S", "M", "L"])
-st.write(a)
+
+st.markdown(
+    """
+    ### how to use
+    1. AI 음성 만들기 페이지에서 음성 생성
+    2. AWS 사용량 조회 페이지에서 사용량 조회
+    
+    """,
+    unsafe_allow_html=True
+)
